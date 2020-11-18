e1be90a24257a5865895e2d28d028ab33e3dd620
damaainan <damaainan@163.com>
2020-11-18 19:18:48 +0800

    todo

--- code/动态规划/0085.php
+++ code/动态规划/0085.php
@@ -45,10 +45,25 @@ cols == matrix.length
 matrix[i][j] 为 '0' 或 '1'
 通过次数46,646提交次数96,129
 */
-function deal(){
+/*
+态规划
+
+heights[i][j]代表[i，j]的高度
+heights[i][j] = matrix[i][j]=='1'? heights[i-1][j] + 1:0
+
+dp[i][j][k] 代表以[i,j]为右下角，高度为k可以组成的面积
+dp[i][j][k] = dp[i][j-1][k] + k  
+
+作者：leeyupeng
+链接：https://leetcode-cn.com/problems/maximal-rectangle/solution/dong-tai-gui-hua-by-leeyupeng-5/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+function deal(array $matrix):int{
 	$start_time = microtime(true);
 	/***************代码开始*********************/
-	
+	$n = count($matrix);
+	$m = count($matrix[0]);
 
 	/***************代码结束*******************/
 	/***********性能监控***********************/ 
@@ -58,5 +73,13 @@ function deal(){
 	/*******************************************/
 	return $res;
 }
-echo deal();
+echo deal([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]);
+echo "\r\n\r\n";
+echo deal([]);
+echo "\r\n\r\n";
+echo deal([["0"]]);
+echo "\r\n\r\n";
+echo deal([["1"]]);
+echo "\r\n\r\n";
+echo deal([["0","0"]]);
 echo "\r\n\r\n";
